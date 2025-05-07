2b4273534174899c95364f4ebaa938b700688b11
DIN SEANGMENG <bongmeng60@gmail.com>
2025-05-07 11:25:24 +0700

    add icon

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,72 +1,90 @@
-import React from 'react';
-import { Layout, Avatar, Badge, Space, Typography,Dropdown } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Layout, Avatar, Badge, Space, Typography, Dropdown } from "antd";
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 import {
   BellOutlined,
   SearchOutlined,
   UserOutlined,
   LogoutOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
 const { Header: AntHeader } = Layout;
 const { Title } = Typography;
+import UserProfile from "../../assets/images/profile.png";
 
-import './style/index.css';
-
+import "./style/index.css";
 
 const Header = ({ children }) => {
-
   const navigate = useNavigate();
-
 
   const items = [
     {
-      key: '1',
+      key: "1",
       label: (
-        <div style={{ display: 'flex', alignItems: 'center' }} onClick={() => navigate('/profile')}>
-         
+        <div
+          style={{ display: "flex", alignItems: "center", gap: "10px" }}
+          onClick={() => navigate("/profile")}
+        >
+          <Icon icon="iconamoon:profile-circle-thin" />
           <p>Profile</p>
         </div>
       ),
-      
     },
     {
-      key: '2',
+      key: "2",
       style: {
-        color: '#1677ff',
+        color: "#1677ff",
       },
       label: (
-        <div style={{ display: 'flex', alignItems: 'center',gap: "10px" }} onClick={() => navigate('/login')}>
-           <LogoutOutlined />
+        <div
+          style={{ display: "flex", alignItems: "center", gap: "10px" }}
+          onClick={() => navigate("/login")}
+        >
+          <LogoutOutlined />
           <p>Login</p>
         </div>
       ),
     },
   ];
   return (
-    <AntHeader style={{ 
-      background: '#fff', 
-      padding: '0 24px', 
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      boxShadow: '0 1px 4px rgba(0, 0, 0, 0.1)'
-    }}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+    <AntHeader
+      style={{
+        background: "#fff",
+        padding: "0 24px",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        boxShadow: "0 1px 4px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
         {children}
-        <Title level={4} style={{ margin: 0, marginLeft: children ? 16 : 0 }}>Dashboard</Title>
+        <Title level={4} style={{ margin: 0, marginLeft: children ? 16 : 0 }}>
+          Dashboard
+        </Title>
       </div>
-      
+
       <Space size="large">
-        <SearchOutlined style={{ fontSize: '18px', cursor: 'pointer' }} />
+        <SearchOutlined style={{ fontSize: "18px", cursor: "pointer" }} />
         <Badge count={5}>
-          <BellOutlined style={{ fontSize: '18px', cursor: 'pointer' }} />
+          <BellOutlined style={{ fontSize: "18px", cursor: "pointer" }} />
         </Badge>
-        <Dropdown className="header-profile" menu={{ items }} trigger={['click']} >
-          <Space  style={{ cursor: 'pointer' }} >
-            <Avatar icon={<UserOutlined />} />
-            <span>John Doe</span>
+        <Dropdown
+         
+          menu={{ items }}
+          trigger={["click"]}
+        >
+          <Space  style={{ cursor: "pointer" }}>
+            <div  className="header-profile" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <img
+                src={UserProfile}
+                alt="user-profile"
+                style={{ width: "30px", height: "30px", borderRadius: "50%" }}
+              />
+              <span>John Doe</span>
+            </div>
           </Space>
         </Dropdown>
       </Space>
@@ -74,4 +92,4 @@ const Header = ({ children }) => {
   );
 };
 
-export default Header; 
+export default Header;
--- src/components/Layouts/Logo.jsx
+++ src/components/Layouts/Logo.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../assets/Logo/logo.png";
+import logo from "../../assets/images/Logo/logo.png";
 import { DoubleLeftOutlined } from "@ant-design/icons";
+import "./style/index.css";
 const Logo = ({ collapsed, setCollapsed, isMobile }) => {
 
 
@@ -25,10 +26,10 @@ const Logo = ({ collapsed, setCollapsed, isMobile }) => {
               alt="logo"
               style={{ width: "30px", height: "30px" }}
             />
-            <h3>Full name</h3>
+            <h3>App Name</h3>
           </div>
           {!isMobile && (
-            <DoubleLeftOutlined onClick={() => setCollapsed(true)} style={{ fontSize: "18px", cursor: "pointer", backgroundColor: "#e6f4ff", borderRadius: "50%", padding: "4px", }} />
+            <DoubleLeftOutlined className="logo-icon" onClick={() => setCollapsed(true)} style={{ fontSize: "18px", cursor: "pointer",  borderRadius: "50%", padding: "4px",transition: "300ms" }} />
           )}
         </div>
       ) : (
